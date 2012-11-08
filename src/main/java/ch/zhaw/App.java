71fb53ac988dd8a8eba79c89aec3742d7968a075
Fabian Kruse <fabian.kruse@a2-c.ch>
2012-11-08 20:51:36 +0100

    StrategyPattern implementiert (Logger)

--- src/main/java/ch/zhaw/App.java
+++ src/main/java/ch/zhaw/App.java
@@ -1,5 +1,6 @@
 package ch.zhaw;
 
+import ch.zhaw.LogImpl.ErrorLog;
 import ch.zhaw.LogImpl.NoLog;
 import ch.zhaw.LogImpl.SimpleLog;
 
@@ -15,6 +16,9 @@ public class App
     	Logger.info("Dies ist ein Infotext");
     	Logger.error("Dies ist ein Errortext");
     	Logger.setLog(new NoLog());
+    	Logger.info("Dies ist ein Infotext");
+    	Logger.error("Dies ist ein Errortext");
+    	Logger.setLog(new ErrorLog());
     	Logger.info("Dies ist ein Infotext");
     	Logger.error("Dies ist ein Errortext");
     }
